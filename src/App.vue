96816011a5e52c2590153fffcc3ce6ed248da4f7
Mary Tionko <mary.tionko@gmail.com>
2023-11-22 23:10:39 +1300

    Creates CardContainer

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,20 @@
 import CardComponent from './components/CardComponent.vue';
 import HelloWorld from './components/HelloWorld.vue';
 import TheWelcome from './components/TheWelcome.vue';
-
-const title = "Attack";
-const body = "Make an attack with a melee weapon, ranged weapon, improvised weapon, or no weapon.";
-const icon = "crossed-swords";
-const title2 = "Cast a spell";
-const body2 = "Cast a spell that has a casting time of 1 (or more) actions. If another spell has been cast as a bonus action, only cantrips are allowed.";
-const icon2 = "spell-book";
+import CardContainer from './components/CardContainer.vue';
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header> -->
 
   <main>
-    <div class="d-flex flex-column gap-3">
-      <CardComponent :title="title" :body="body" :iconType="icon"/>
-      <CardComponent :title="title2" :body="body2" :iconType="icon2"/>
-    </div>
+    <CardContainer />
     <!-- <TheWelcome /> -->
   </main>
 </template>
---
+++ src/components/CardContainer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import CardComponent from './CardComponent.vue';
+  const title = "Attack";
+  const body = "Make an attack with a melee weapon, ranged weapon, improvised weapon, or no weapon.";
+  const icon = "crossed-swords";
+  const title2 = "Cast a spell";
+  const body2 = "Cast a spell that has a casting time of 1 (or more) actions. If another spell has been cast as a bonus action, only cantrips are allowed.";
+  const icon2 = "spell-book";
+</script>
+
+<template>
+  <div class="d-flex flex-column gap-3">
+    <CardComponent :title="title" :body="body" :iconType="icon" />
+    <CardComponent :title="title2" :body="body2" :iconType="icon2" />
+  </div>
+</template>
