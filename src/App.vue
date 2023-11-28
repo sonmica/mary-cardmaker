2b9ce6e11167d45c38510a61d323c3a556d66e1c
Mary Tionko <mary.tionko@gmail.com>
2023-11-28 22:58:23 +1300

    Cleans up unused vue templated code

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <script setup lang="ts">
-import CardComponent from './components/CardComponent.vue';
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
 import CardContainer from './components/CardContainer.vue';
 </script>
 
-<template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header> -->
-  
+<template>  
   <CardContainer />
-
-  <!-- <main>
-    <TheWelcome />
-  </main> -->
 </template>
 
 <style scoped>
