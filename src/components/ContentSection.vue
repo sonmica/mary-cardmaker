e1ff12555b26a6f2396f8c4b3839933c6c982daa
Mary Tionko <mary.tionko@gmail.com>
2023-11-29 22:50:13 +1300

    Adds column arround component to form gutters on main page

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -15,13 +15,16 @@ console.log("content section - child cards", props.section);
     <h2 class="text-uppercase">{{ sectionTitle }}</h2>
   </div>
   
-  <div class="row row-cols-2 g-3">
-    <CardComponent
-      v-for="card in section.contents"
-      :key="card.id" :title="card.name"
-      :body="card.description"
-      :iconType="card.iconType"
-      :subBody="card.usage ? `Usage: ${card.usage}` : undefined"
-      class="col"/>
+  <div class="row g-3">
+
+    <div class="col-6"
+        v-for="card in section.contents"
+        :key="card.id">
+
+      <CardComponent :title="card.name"
+        :body="card.description"
+        :iconType="card.iconType"
+        :subBody="card.usage ? `Usage: ${card.usage}` : undefined"/>
+    </div>
   </div>
 </template>
