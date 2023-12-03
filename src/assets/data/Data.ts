a015b1e84afa326155dc8364040caefc0f3461b4
sonmica <sonmica@gmail.com>
2023-12-04 02:13:06 +1300

    Includes player configuration in main data file

--- src/assets/data/Data.ts
+++ src/assets/data/Data.ts
@@ -1,12 +1,14 @@
 import backgrounds from './backgrounds/Backgrounds';
 import classes from './classes/Classes';
 import commonActions from './commonActions.json';
+import players from './players.json';
 import races from './races/Races';
 
 const Data = {
   backgrounds,
   classes,
   commonActions,
+  players,
   races
 };
 
