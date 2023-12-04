4516e64c1ca5b5c66f20343931851661ea18914f
Mary Tionko <mary.tionko@gmail.com>
2023-12-05 00:23:30 +1300

    Adds parser to icon generator

--- generate-icons.js
+++ generate-icons.js
@@ -6,13 +6,21 @@ const TYPE = `${TARGET_FOLDER}/IconType.ts`;
 
 let baseTypes = `export type IconType =`;
 
+const kebabToCamelCase = text => {
+  const splitText = text.split("-");
+          
+  return splitText
+    .map(t => `${t.substring(0,1).toUpperCase()}${t.substring(1, t.length)}`)
+    .reduce((prev, curr) => `${prev}${curr}`, "");
+}
+
 const generateIconFile = svg => {
     const baseName = svg.substring(0, svg.length - 4);
     const contents =  `<template>${readFileSync(`${ICONS_FOLDER}/${svg}`, 'utf-8')}</template>`;
 
     baseTypes += `\n  | '${baseName}'`
 
-    writeFileSync(`${TARGET_FOLDER}/${baseName}.vue`, contents);
+    writeFileSync(`${TARGET_FOLDER}/Icon${kebabToCamelCase(baseName)}.vue`, contents);
 }
 
 // cleanup previous run
